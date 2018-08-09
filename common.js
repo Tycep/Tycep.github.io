b2d8cb1dda6cc955e05a6ceeb31200f82105f441
Tycep <33693790+Tycep@users.noreply.github.com>
2018-08-09 20:08:58 +0300

    Add files via upload

--- common.js
+++ common.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
+
 $(document).ready(function(){
 	$('.intro .btn').click(function valid(){
 	$('html, body').animate({scrollTop: $('#text').offset().top},1750);
 	});
-
-=======
-$(document).ready(function(){
-	$('.intro .btn').click(function valid(){
-	$('html, body').animate({scrollTop: $('#text').offset().top},1750);
-	});
-
->>>>>>> origin/master
 });
